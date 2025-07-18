0bca33de4da177858a09c67407db4841ee129f77
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-07-18 10:48:38 +0100

    close menu icon added to index.html, 'click'cevent created to remove 'open' class

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let btn = document.querySelector('.bars')
+let openBtn = document.querySelector('.bars')
+let closeBtn = document.querySelector('.fa-times')
 let menu = document.querySelector('.menu')
 
-btn.addEventListener('click', () => {
+
+openBtn.addEventListener('click', () => {
     menu.classList.add('open')
+})
+
+closeBtn.addEventListener('click', () => {
+    menu.classList.remove('open')
 })
